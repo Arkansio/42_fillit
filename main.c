f96f39d6f54b586413a06e457c72a14ed694e00e
Arkansio <38956983+Arkansio@users.noreply.github.com>
2018-11-25 16:33:57 +0100

    add performance records

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 15:46:12 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/23 21:56:22 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/25 16:32:20 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+#include <time.h>
 
 static void		ft_init(char *file)
 {
@@ -26,8 +28,14 @@ static void		ft_init(char *file)
 
 int		main(int argc, char **argv)
 {
+	clock_t begin = clock();
+
 	if (argc == 2)
 		ft_init(argv[1]);
 	else
 		ft_putstr_fd("Usage: <file>\n", 1);
+
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time spend: %fs\n", time_spent);
 }
