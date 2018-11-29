0a8dc8855a0933376354ce005cab1e4fe5d49673
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-11-29 01:05:56 +0100

    calcul the perfect square for ous fillit

--- ft_resolve.c
+++ ft_resolve.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 23:48:36 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/29 01:00:13 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/29 01:05:38 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 int         ft_resolve(t_map *map)
 {
     int     size;
+    int     **tab;
 
-    size = ft_perfect_square(123);
+    size = ft_perfect_square(map->nb_pcs * 4);
     printf("nb blocks: %d\n", map->nb_pcs);
     printf("%d\n", size);
     return (1);
