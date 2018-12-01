fc35babcebaac7d1b4f9a14fd791617b0a3e27ef
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-01 01:50:27 +0100

    simplify perfect square

--- ft_perfect_square.c
+++ ft_perfect_square.c
@@ -6,18 +6,16 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 23:50:51 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/30 23:30:19 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/01 01:45:42 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_perfect_square(int nb)
 {
-	float	c;
+	int		c;
 
-	c = 0.f;
+	c = 0;
 	while (c * c < nb)
-		c += 0.25f;
-	if ((int)((float)(int)c - c) == 0)
-		return (int)(c);
-	return (int)(c + 1);
+		c++;
+	return (c);
 }
